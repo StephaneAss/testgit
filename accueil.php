126b44a9874bd9ca9e97904480467dc6af18e3f6
Stephane <mensste30@gmail.com>
2019-11-27 18:35:05 +0100

    Mon premier

---
+++ accueil.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	 in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</body>
+</html>
